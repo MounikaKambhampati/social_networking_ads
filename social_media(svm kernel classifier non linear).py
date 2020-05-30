5e36f48575dedee4723e126bf54d6bd740aaa5f8
MounikaKambhampati <62988150+MounikaKambhampati@users.noreply.github.com>
2020-05-30 06:19:22 -0700

    Add files via upload

---
+++ social_media(svm kernel classifier non linear).py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 26 03:08:18 2020
+
+@author: Lenovo
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
+from matplotlib.colors import ListedColormap
+
+# Importing the dataset
+
+
+datasets = pd.read_csv('Social_Network_Ads.csv')
+X = datasets.iloc[:, [2,3]].values
+Y = datasets.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and Test set
+
+
+X_Train, X_Test, Y_Train, Y_Test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+
+
+sc_X = StandardScaler()
+X_Train = sc_X.fit_transform(X_Train)
+X_Test = sc_X.transform(X_Test)
+
+# Fitting the classifier into the Training set
+
+
+
+clas = SVC(kernel = 'linear', random_state = 0)
+clas.fit(X_Train, Y_Train)
+
+# Predicting the test set results
+
+Y_Pred = clas.predict(X_Test)
+
+# Making the Confusion Matrix 
+
+
+cm = confusion_matrix(Y_Test, Y_Pred)
+
+# Visualising the Training set results
+
+
+X_Set, Y_Set = X_Train, Y_Train
+X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_Set[:, 1].min() - 1, stop = X_Set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clas.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'black')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_Set)):
+    plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
+                c = ListedColormap(('red', 'black'))(i), label = j)
+plt.title('SVM(training set)')
+plt.xlabel('Age')
+plt.ylabel('EstimatedSalary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+
+
+X_Set, Y_Set = X_Test, Y_Test
+X1, X2 = np.meshgrid(np.arange(start = X_Set[:, 0].min() - 1, stop = X_Set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_Set[:, 1].min() - 1, stop = X_Set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, clas.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('blue', 'black')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(Y_Set)):
+    plt.scatter(X_Set[Y_Set == j, 0], X_Set[Y_Set == j, 1],
+                c = ListedColormap(('blue', 'black'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('EstimatedSalary')
+plt.legend()
+plt.show()
+  
+
+
